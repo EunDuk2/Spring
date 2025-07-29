226163e4811164096a17ba1144ad2aeb1dd77044
EunDuk2 <dmsqls6789@naver.com>
2025-07-29 14:13:53 +0900

    Redis 도입 및 로그인 기능 완료

--- ordersystem/src/main/java/com/beyond/ordersystem/common/auth/JwtTokenProvider.java
+++ ordersystem/src/main/java/com/beyond/ordersystem/common/auth/JwtTokenProvider.java
@@ -8,17 +8,21 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 @Component
-@RequiredArgsConstructor
 public class JwtTokenProvider {
     private final MemberRepository memberRepository;
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Value("${jwt.expirationAt}")
     private int expirationAt;
@@ -34,6 +38,13 @@ public class JwtTokenProvider {
 
     private Key secret_at_key;
     private Key secret_rt_key;
+
+    // Qualifier는 기본적으로 메서드를 통한 주입 가능. 그래서, 이 경우 생성자 주입 방식을 해야 Qualifier 사용 가능.
+    @Autowired
+    public JwtTokenProvider(MemberRepository memberRepository, @Qualifier("rtInventory") RedisTemplate<String, String> redisTemplate) {
+        this.memberRepository = memberRepository;
+        this.redisTemplate = redisTemplate;
+    }
 
     // 스프링 빈이 만들어지는 시점에 빈이 만들어진 직후에 아래 메서드가 바로 실행
     @PostConstruct
@@ -83,7 +94,10 @@ public class JwtTokenProvider {
                 .signWith(secret_rt_key)
                 .compact();
 
-        // rt 토큰을 redis에 저장
+        // rt 토큰을 redis에 저장 : key-value 형식으로 set
+        redisTemplate.opsForValue().set(member.getEmail(), refreshToken);
+        redisTemplate.opsForValue().set(member.getEmail(), refreshToken, 200, TimeUnit.DAYS); // 200일 ttl
+
 
         return refreshToken;
     }
@@ -100,6 +114,10 @@ public class JwtTokenProvider {
         Member member = memberRepository.findByEmail(email).orElseThrow(() -> new EntityNotFoundException("없는 사용자"));
 
         // redis의 값과 비교하는 검증
+        String redisRt = redisTemplate.opsForValue().get(member.getEmail());
+        if(!redisRt.equals(refreshToken)) {
+            throw new IllegalArgumentException("잘못된 토큰입니다.");
+        }
 
         return member;
     }
