4e52882ae91060611b8e3c1cb3b2634aeb174e9f
EunDuk2 <dmsqls6789@naver.com>
2025-08-01 15:45:19 +0900

    pub/sub 기능 사용하여 message 전파 기능까지 구현

--- ordersystem/src/main/java/com/beyond/ordersystem/common/config/RedisConfig.java
+++ ordersystem/src/main/java/com/beyond/ordersystem/common/config/RedisConfig.java
@@ -1,14 +1,19 @@
 package com.beyond.ordersystem.common.config;
 
+import com.beyond.ordersystem.common.service.SseAlarmService;
 import com.fasterxml.jackson.databind.JsonSerializable;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -78,15 +83,36 @@ public class RedisConfig {
 
     @Bean
     @Qualifier("ssePubSub")
-    public RedisTemplate<String, String> sseRedisTemplate(@Qualifier("ssePubSub") RedisConnectionFactory stockConnectionFactory) {
+    public RedisTemplate<String, String> sseRedisTemplate(@Qualifier("ssePubSub") RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(stockConnectionFactory);
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
         return redisTemplate;
     }
 
     // redis 리스너 객체
+    @Bean
+    @Qualifier("ssePubSub")
+    public RedisMessageListenerContainer redisMessageListenerContainer(
+            @Qualifier("ssePubSub") RedisConnectionFactory redisConnectionFactory,
+            MessageListenerAdapter messageListenerAdapter
+    ) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
 
+        container.setConnectionFactory(redisConnectionFactory);
+        // 아래 채널에서 오는 메시지를 해당 객체로 낚아채겠다?
+        container.addMessageListener(messageListenerAdapter, new PatternTopic("order-channel"));
+
+        return container;
+    }
+
+    // redis의 채널에서 수신된 메시지를 처리하는 빈객체 (위에서 수신한걸 여기서 처리한다.)
+    @Bean
+    public MessageListenerAdapter messageListenerAdapter(SseAlarmService sseAlarmService) {
+        // 채널로부터 수신되는 message처리를 SseAlarmService의 onMessage메서드로 설정
+        // 즉, 메시지가 수신되면 onMessage메서드가 호출된다.
+        return new MessageListenerAdapter(sseAlarmService, "onMessage");
+    }
 
 }
