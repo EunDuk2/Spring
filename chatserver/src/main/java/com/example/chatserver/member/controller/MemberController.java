f1dc3e9d6224ec1f668c3344fee2444fe3476729
EunDuk2 <dmsqls6789@naver.com>
2025-10-06 23:55:54 +0900

    회원 목록 조회 기능 구현

--- chatserver/src/main/java/com/example/chatserver/member/controller/MemberController.java
+++ chatserver/src/main/java/com/example/chatserver/member/controller/MemberController.java
@@ -4,16 +4,15 @@ import com.example.chatserver.common.auth.JwtTokenProvider;
 import com.example.chatserver.member.domain.Member;
 import com.example.chatserver.member.dto.MemberLoginReqDto;
 import com.example.chatserver.member.dto.MemberSaveReqDto;
+import com.example.chatserver.member.dto.MemberlistResDto;
 import com.example.chatserver.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -39,5 +38,11 @@ public class MemberController {
         loginInfo.put("token", jwtToken);
 
         return new ResponseEntity(loginInfo, HttpStatus.OK);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<?> memberList() {
+        List<MemberlistResDto> dtos = memberService.findAll();
+        return new ResponseEntity(dtos, HttpStatus.OK);
     }
 }
--- chatserver/src/main/java/com/example/chatserver/member/service/MemberService.java
+++ chatserver/src/main/java/com/example/chatserver/member/service/MemberService.java
@@ -3,12 +3,16 @@ package com.example.chatserver.member.service;
 import com.example.chatserver.member.domain.Member;
 import com.example.chatserver.member.dto.MemberLoginReqDto;
 import com.example.chatserver.member.dto.MemberSaveReqDto;
+import com.example.chatserver.member.dto.MemberlistResDto;
 import com.example.chatserver.member.repository.MemberRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -38,5 +42,18 @@ public class MemberService {
             throw new IllegalArgumentException("비밀번호가 틀립니다.");
         }
         return member;
+    }
+
+    public List<MemberlistResDto> findAll() {
+        List<Member> members = memberRepository.findAll();
+        List<MemberlistResDto> memberlistResDtos = new ArrayList<>();
+        for (Member member : members) {
+            MemberlistResDto memberlistResDto = new MemberlistResDto();
+            memberlistResDto.setId(member.getId());
+            memberlistResDto.setName(member.getName());
+            memberlistResDto.setEmail(member.getEmail());
+            memberlistResDtos.add(memberlistResDto);
+        }
+        return memberlistResDtos;
     }
 }
