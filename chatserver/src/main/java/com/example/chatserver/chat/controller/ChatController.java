51d0d99c7a6ae257a5aab97cad562751ad498ccf
EunDuk2 <dmsqls6789@naver.com>
2025-10-07 21:44:20 +0900

    채팅방 생성 api 구현

--- chatserver/src/main/java/com/example/chatserver/chat/controller/ChatController.java
+++ chatserver/src/main/java/com/example/chatserver/chat/controller/ChatController.java
@@ -2,7 +2,11 @@ package com.example.chatserver.chat.controller;
 
 import com.example.chatserver.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ChatController {
     private final ChatService chatService;
+
+    // 그룹채팅방 개설
+    @PostMapping("/room/group/create")
+    public ResponseEntity<?> createGroupRoom(@RequestParam String roomName) {
+        chatService.createGroupRoom(roomName);
+        return ResponseEntity.ok().build();
+    }
 }
--- chatserver/src/main/java/com/example/chatserver/chat/service/ChatService.java
+++ chatserver/src/main/java/com/example/chatserver/chat/service/ChatService.java
@@ -14,6 +14,7 @@ import com.example.chatserver.member.repository.MemberRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -52,8 +53,27 @@ public class ChatService {
                     .chatMessage(chatMessage)
                     .isRead(c.getMember().equals(sender))
                     .build();
+            System.out.println("eunsung: " + c.getMember().equals(sender));
             readStatusRepository.save(readStatus);
         }
 
+    }
+
+    public void createGroupRoom(String chatRoomName) {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        Member member = memberRepository.findByEmail(email).orElseThrow(() -> new EntityNotFoundException("member cannot found"));
+
+        // 채팅방 생성
+        ChatRoom chatRoom = ChatRoom.builder()
+                .name(chatRoomName)
+                .isGroupChat("Y")
+                .build();
+        chatRoomRepository.save(chatRoom);
+        // 채팅 참여자로 개설자를 추가
+        ChatParticipant chatParticipant = ChatParticipant.builder()
+                .chatRoom(chatRoom)
+                .member(member)
+                .build();
+        chatParticipantRepository.save(chatParticipant);
     }
 }
